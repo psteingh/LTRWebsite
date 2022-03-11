28dfa3e5b679d8a1050688f79e6295826a48d596
psteingh <peter3stein@gmail.com>
2022-03-11 09:49:22 -0500

    ltr0311949

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ require("./routes/ltrlie.routes")(app);
 require("./routes/contact.routes")(app);
 
 // if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, './client/public')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/public', 'index.html'));
 });
 
 // code for BHSNA only
