017a5428a91d0c56e992890afeb8a431a9fdbfc1
psteingh <peter3stein@gmail.com>
2022-03-11 14:03:04 -0500

    ltr0311203

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
