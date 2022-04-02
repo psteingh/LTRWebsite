821540203ce05b39399fc76abdfe9dfb084ce7e4
psteingh <peter3stein@gmail.com>
2022-04-02 11:43:20 -0400

    ltr04021143

--- client/src/components/lierboard.component.js
+++ client/src/components/lierboard.component.js
@@ -6,21 +6,22 @@ import LtrLieDataService from "../services/ltrlie.service";
 export default class LierBoard extends Component {
   constructor(props) {
     super(props);
-    this.retrieveLtrLies = this.retrieveLtrLies.bind(this);
+    this.updatePublished = this.updatePublished.bind(this);
 
     this.state = {
       content: "",
       ltrlies: [],
       currentLtrLie: {
+        id: null,
+        name: "",
         subject: "",
+        stuff: "",
         published: false,
       },
     };
   }
 
   componentDidMount() {
-    this.retrieveLtrLies();
-
     UserService.getPublicContent().then(
       response => {
         this.setState({
@@ -38,11 +39,20 @@ export default class LierBoard extends Component {
     );
   }
 
-  retrieveLtrLies() {
-    LtrLieDataService.getAll()
+  updatePublished(status) {
+    var data = {
+      id: this.state.currentLtrLie.id,
+      name: this.state.currentLtrLie.name,
+      subject: this.state.currentLtrLie.subject,
+      stuff: this.state.currentLtrLie.stuff,
+      published: status
+    }
+
+    LtrLieDataService.update(this.state.currentLtrLie.id, data)
       .then(response => {
         this.setState({
           ltrlies: response.data,
+          published: status,
         });
         console.log(response.data);
       })
@@ -50,7 +60,6 @@ export default class LierBoard extends Component {
         console.log(e);
       });
   }
-
 
   render() {
     const { currentLtrLie } = this.state;
@@ -72,7 +81,6 @@ export default class LierBoard extends Component {
                 {currentLtrLie.name}
               </div>
             </div>
-          )
         </div>
       </div>
     );
