2f2a4d463aa0c90d1b477bc0c86a0ac4c15a7dfa
psteingh <peter3stein@gmail.com>
2022-04-06 07:40:15 -0400

    ltr0406740

--- client/src/components/lierboard.component.js
+++ client/src/components/lierboard.component.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 import UserService from "../services/user.service";
 import LtrLieDataService from "../services/ltrlie.service";
 
-import LieList from "../home/lielist";
+// import LieList from "../home/lielist";
+
 export default class LierBoard extends Component {
   constructor(props) {
     super(props);
@@ -63,14 +64,85 @@ export default class LierBoard extends Component {
   }
 
   render() {
+    const { ltrlies, currentLtrLie, currentIndex } = this.state;
+
     return (
       <div className="container">
-
         {/* <div className="notice-sec opake">
           <h3>{this.state.content}</h3>
         </div> */}
+        {/* <LieList /> */}
 
-        <LieList />
+        <div className="col-md-6">
+          <h4>List of Lies</h4>
+          <ul className="list-group">
+            {ltrlies &&
+              ltrlies.map((ltrlie, index) => (
+              <li
+                className={
+                "list-group-item " +
+                (index === currentIndex ? "active" : "")
+                }
+                onClick={() => this.setActiveLtrLie(ltrlie, index)}
+                key={index}>
+                
+                {ltrlie.name}
+              </li>
+              ))}
+          </ul>
+          
+          <p><button className="submit-button">
+              Add a Lie</button></p>
+
+          <p><button className="submit-button"
+              onClick={this.refreshPage}>
+            List of Lies
+          </button></p>
+        </div>
+
+        <div className="col-md-6">
+          {currentLtrLie ? (
+            <div>
+              <h5>Lie details</h5>
+              <div>
+                <label>
+                  <strong>Name:</strong>
+                </label>{" "}
+                {currentLtrLie.name}
+              </div>
+              <div>
+                <label>
+                <strong>Subject:</strong>
+                </label>{" "}
+                {currentLtrLie.subject}
+              </div>
+              <div>
+                <label>
+                <strong>Stuff:</strong>
+                </label>{" "}
+                {currentLtrLie.stuff}
+              </div>
+              <div>
+                <label>
+                  <strong>Status:</strong>
+                </label>{" "}
+                {currentLtrLie.published ? "Published" : "Pending"}
+              </div>
+              <button className="submit-button">
+                Edit</button>            
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Click on Lie for details</p>
+            </div>
+          )}
+        </div>
+
+
+
+
+
       </div>
     );
   }
