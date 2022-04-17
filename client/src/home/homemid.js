edeb327edd87c98eb24ea4f076c21139fbd13cbe
psteingh <peter3stein@gmail.com>
2022-04-17 15:00:07 -0400

    ltr0417300

--- client/src/home/homemid.js
+++ client/src/home/homemid.js
@@ -7,18 +7,18 @@ function HomeMid(){
     return(
         <div className="grid-container">
         <div className="grid-child">
-            <h1 className="cd-header">Catalog</h1>
-            <div className="cd-text">Public</div>
+            <h1 className="cd-header">Lier Board</h1>
+            <div className="cd-text">Public ...see other lies, rate, edit, advice</div>
             <img className="cd-image" src={ltrcatalog} alt="LTR Catalog" />
         </div>
         <div className="grid-child">
-            <h1 className="cd-header">Vault</h1>
-            <div className="cd-text">Private</div>
+            <h1 className="cd-header">List of Lies</h1>
+            <div className="cd-text">Private ...storing lies, password protected</div>
             <img className="cd-image" src={ltrvault} alt="LTR Vault" />
         </div>
         <div className="grid-child">
             <h1 className="cd-header">Videos</h1>
-            <div className="cd-text">Watch...coming soon</div>
+            <div className="cd-text">Watch ...see lies in action, upload, coming soon</div>
             <img className="cd-image" src={ltrvideos} alt="LTR Videos" />
         </div>
         </div>
