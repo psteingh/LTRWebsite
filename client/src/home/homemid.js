1aa2f98bfe797aec02e58e2ef4e9f00b08017eaf
psteingh <peter3stein@gmail.com>
2022-04-17 14:19:39 -0400

    ltr0417219

--- client/src/home/homemid.js
+++ client/src/home/homemid.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ltrcatalog from "../images/ltrcatalog.png";
 import ltrvault from "../images/ltrvault.png";
+import ltrvideos from "../images/ltrvideos.png";
 
 function HomeMid(){
     return(
-        <div>
-
-        <img src={ltrcatalog} alt="LTR Catalog" />
-
-        <img src={ltrvault} alt="LTR Vault" />
-
+        <div className="grid-container">
+        <div className="grid-child">
+            <h1>Catalog</h1>
+            <div>Public</div>
+            <img className="cd-image" src={ltrcatalog} alt="LTR Catalog" />
+        </div>
+        <div className="grid-child"><img className="cd-image" src={ltrvault} alt="LTR Vault" />
+        </div>
+        <div className="grid-child"><img className="cd-image" src={ltrvideos} alt="LTR Videos" />
+        </div>
         </div>
     );
 }
