487e0e018b07a45d652aa1f2e00c00e948f44eea
psteingh <peter3stein@gmail.com>
2022-05-14 12:23:15 -0400

    ltr05141223

--- client/src/home/homemid.js
+++ client/src/home/homemid.js
@@ -7,12 +7,12 @@ function HomeMid(){
         <div className="grid-container">
         <div className="grid-child">
             <h1 className="cd-header">Lier Board</h1>
-            <div className="cd-text">Public ...see list of lies signed up users have made public</div>
+            <div className="cd-text">Public ...see list of lies, signed up Users have made public</div>
             <img className="cd-image" src={ltrcatalog} alt="LTR Catalog" />
         </div>
         <div className="grid-child">
             <h1 className="cd-header">List of Lies</h1>
-            <div className="cd-text">Private ...after signing up users can store lies for future reference</div>
+            <div className="cd-text">Private ...after signing up, Users can store lies for future reference</div>
             <img className="cd-image" src={ltrvault} alt="LTR Vault" />
         </div>
         
