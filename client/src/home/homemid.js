7be1555b875fefeca441d7ffa7d4337a17b488c3
psteingh <peter3stein@gmail.com>
2022-05-14 13:14:55 -0400

    ltr0514114

--- client/src/home/homemid.js
+++ client/src/home/homemid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ltrcatalog from "../images/ltrcatalog.png";
 import ltrvault from "../images/ltrvault.png";
 
@@ -6,14 +7,18 @@ function HomeMid(){
     return(
         <div className="grid-container">
         <div className="grid-child">
+            <Link to={"/lierboard"}>
             <h1 className="cd-header">Lier Board</h1>
-            <div className="cd-text">Public ...see list of lies, signed up Users have made public</div>
+            <div className="cd-text">Public ...see list of lies signed up Users have made public</div>
             <img className="cd-image" src={ltrcatalog} alt="LTR Catalog" />
+            </Link>
         </div>
         <div className="grid-child">
+            <Link to={"/login"}>
             <h1 className="cd-header">List of Lies</h1>
-            <div className="cd-text">Private ...after signing up, Users can store lies for future reference</div>
+            <div className="cd-text">Private ...after logging in, Users can store lies for future reference</div>
             <img className="cd-image" src={ltrvault} alt="LTR Vault" />
+            </Link>
         </div>
         
         </div>
