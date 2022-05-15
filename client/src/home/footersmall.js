a8850ebc80ecd28fe3fb4a3b6f4896bffa8ab09d
psteingh <peter3stein@gmail.com>
2022-05-15 07:40:11 -0400

    ltr0515740

---
+++ client/src/home/footersmall.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ltrbrand from "../images/ltrbrand.png";
+
+function Footer(){
+  return(
+  <footer className="dblue footer-sec">
+
+    <div className="footertext-sec">
+      <div className="fmess-text">
+        Due to limited world-wide server space,
+        lawyers & politicians are not allowed to use this site
+      </div>
+    </div>
+    
+    <div className="footertext-sec">
+      <div className="fabout-text">
+        <Link to="/">
+          <img className="logo-ftr" src={ltrbrand} alt="LTR logo" />
+        </Link>
+      </div>
+      <div className="footerbutton-sec">
+        <button className="submit-button">
+        <Link to={"/contact"}>
+          Contact Us
+        </Link>
+        </button>
+      </div>
+    </div>
+
+  </footer>
+
+  );
+}
+
+export default Footer;
