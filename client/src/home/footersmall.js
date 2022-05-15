db113943c947653edb1ba62075efa4cd64e361c6
psteingh <peter3stein@gmail.com>
2022-05-15 10:27:40 -0400

    ltr05151027

--- client/src/home/footersmall.js
+++ client/src/home/footersmall.js
@@ -4,18 +4,9 @@ import ltrbrand from "../images/ltrbrand.png";
 
 function FooterSmall(){
   return(
-  <div className="container">
-  <div className="dblue footersmall-sec">
-  <div className="list row">
-
-    <div className="footersmalltext-sec">
-      <div className="fmesssmall-text">
-        Due to limited world-wide server space,
-        lawyers & politicians are not allowed to use this site
-      </div>
-    </div>
+  <div className="grid-container dblue footersmall-sec">
     
-    <div className="col-md-6">
+    <div className="grid-child">
       <div className="footersmalltext-sec">
         <div className="faboutsmall-text">
         <Link to="/">
@@ -25,7 +16,7 @@ function FooterSmall(){
       </div>
     </div>
 
-    <div className="col-md-6">
+    <div className="grid-child">
       <div className="footersmallbutton-sec">
         <button className="submit-button">
         <Link to={"/contact"}>
@@ -35,8 +26,6 @@ function FooterSmall(){
       </div>
     </div>
       
-  </div>
-  </div>
   </div>
 
   );
