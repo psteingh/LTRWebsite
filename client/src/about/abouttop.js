3d224369c0520c5cab5d7d414349032077826340
psteingh <peter3stein@gmail.com>
2022-05-22 11:16:07 -0400

    ltr05221116

--- client/src/about/abouttop.js
+++ client/src/about/abouttop.js
@@ -3,8 +3,13 @@ import ltrdeceive from "../images/ltrdeceive.png";
 
 function AboutTop(){
     return(
-        <div>
+        <div className="abtop-container">
           <img className="ab-image" src={ltrdeceive} alt="LTR Deceive" />  
+        
+        <div className="abtext-centered">
+          About Us
+        </div>
+        
         </div>
     );
 }
