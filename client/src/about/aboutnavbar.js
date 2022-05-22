bce6ecdb05320b2eb6534041a1bedee88804c7e8
psteingh <peter3stein@gmail.com>
2022-05-22 10:31:45 -0400

    ltr05221031

--- client/src/about/aboutnavbar.js
+++ client/src/about/aboutnavbar.js
@@ -2,46 +2,11 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ltrbrand from "../images/ltrbrand.png";
 
-import AuthService from "../services/auth.service";
-import EventBus from "../services/eventbus";
-
 class AboutNavbar extends Component {
-    constructor(props) {
-      super(props);
-      this.logOut = this.logOut.bind(this);
-  
-      this.state = {
-        currentUser: undefined,
-      };
-    }
-
-    componentDidMount() {
-        const user = AuthService.getCurrentUser();
-
-    if (user) {
-      this.setState({
-        currentUser: user,
-      });
-    }
-    EventBus.on("logout", () => {
-        this.logOut();
-      });
-    }
-    componentWillUnmount() {
-        EventBus.remove("logout");
-      }
-      
-        logOut() {
-          AuthService.logout();
-          this.setState({
-            currentUser: undefined,
-          });
-        }
-
 render() {
     return (
       <div>
-        <nav className="blacktowhite navbar navbar-expand navbar-dark">
+        <nav className="blacktowhite navbar">
           
           <Link to={"/"} className="navbar-brand">
           <img className="logo-ftr" src={ltrbrand} alt="LTR logo" />
