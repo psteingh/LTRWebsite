886fa3dac3e7081ca2bb9126060fa238f48baddf
psteingh <peter3stein@gmail.com>
2022-05-23 08:16:15 -0400

    ltr0523816

--- client/src/home/carousel.js
+++ client/src/home/carousel.js
@@ -3,16 +3,16 @@ import Carousel from "react-bootstrap/Carousel";
 function CarouselBS() {
   return (
 <Carousel>
-  <Carousel.Item interval={5000}>
+  <Carousel.Item interval={3000}>
     <Carousel.Caption>When did I tell them a Lie?</Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item interval={5000}>
+  <Carousel.Item interval={3000}>
     <Carousel.Caption>Did I tell them a Lie?</Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item interval={5000}>
+  <Carousel.Item interval={3000}>
     <Carousel.Caption>What Lie did I tell them?</Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item interval={5000}>
+  <Carousel.Item interval={3000}>
     <Carousel.Caption>Why did I tell them a Lie?</Carousel.Caption>
   </Carousel.Item>
 </Carousel>
