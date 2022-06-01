b3c3a18d232932821dd403d4933ec959371e0c8f
psteingh <peter3stein@gmail.com>
2022-06-01 12:24:08 -0400

    ltr06011224

--- client/src/liesbible/liesbibtop.js
+++ client/src/liesbible/liesbibtop.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ltrblackbkgrnd from "../images/ltrblackbkgrnd.png";
+import ltrbible from "../images/ltrbible.png";
 
 function LiesBibleTop(){
     return(
         <div className="liestop-container">
-          <img className="liesab-image" src={ltrblackbkgrnd} alt="LTR Black background" />  
+          <img className="liesab-image" src={ltrbible} alt="LTR Bible" />  
         
         <div className="liestext-centered">
           Biblical Lies
