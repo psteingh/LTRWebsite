b3e00d920bd44e3d25c270a9628fa27bba61abf0
psteingh <peter3stein@gmail.com>
2022-06-01 13:14:51 -0400

    ltr0601114

--- client/src/liesmedia/liesmedtop.js
+++ client/src/liesmedia/liesmedtop.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ltrblackbkgrnd from "../images/ltrblackbkgrnd.png";
+import ltrmedia from "../images/ltrmedia.png";
 
 function LiesMediaTop(){
     return(
         <div className="liestop-container">
-          <img className="liesab-image" src={ltrblackbkgrnd} alt="LTR Black background" />  
+          <img className="liesab-image" src={ltrmedia} alt="LTR Media" />  
         
         <div className="liestext-centered">
           Media Lies
