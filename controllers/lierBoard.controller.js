c69258699af3385b8495b872f2c603ed18839597
psteingh <peter3stein@gmail.com>
2022-06-26 14:12:08 -0400

    ltr0626212

--- controllers/lierBoard.controller.js
+++ controllers/lierBoard.controller.js
@@ -18,20 +18,55 @@ exports.findAllPublished = (req, res) => {
   });
 };
 
-// Sort all published LtrLies
 exports.sortAllPublished = (req, res) => {
-  // const arr = { name: req.body.name };
-  
-  LtrLie.find({sort: {name: req.body.name}})
-  .then(data => {
-    console.log(data);
+  const match = {}
+  const sort = {}
 
-    res.send(data);
-  })
-  .catch(err => {
+  if(req.query.published){
+    match.published = req.query.published === 'true'
+}
+
+  if(req.query.sortBy && req.query.OrderBy){
+    sort[req.query.sortBy]   = req.query.OrderBy === 'desc' ? -1 : 1
+  }
+
+  LtrLie()
+  try {
+    req.user.populate({
+        // path:'posts',
+        match,
+        options:{
+            // limit: parseInt(req.query.limit),
+            // skip: parseInt(req.query.skip),
+            sort
+        }
+    }).execPopulate()
+    res.send(data)
+    console.log(data);
+  }
+  catch(error) {
     res.status(500).send({
       message:
         err.message || "Some error occurred while retrieving ltrlies."
       });
-  });
+  };
 };
+
+
+
+// Sort all published LtrLies
+// exports.sortAllPublished = (req, res) => {
+  
+//   LtrLie.find()
+//   .then(data => {
+//     console.log(data);
+
+//     res.send(data);
+//   })
+//   .catch(err => {
+//     res.status(500).send({
+//       message:
+//         err.message || "Some error occurred while retrieving ltrlies."
+//       });
+//   });
+// };
