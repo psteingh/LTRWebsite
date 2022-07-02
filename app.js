dd4d2910533e9996ebbf46b11f834245fc49a800
psteingh <peter3stein@gmail.com>
2022-07-02 08:16:14 -0400

    ltr0702816

--- app.js
+++ app.js
@@ -61,15 +61,15 @@ function initial() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       
-      // new Role({
-      //   name: "user"
-      // }).save(err => {
-      //   if (err) {
-      //     console.log("error", err);
-      //   }
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
 
-      //   console.log("added 'user' to roles collection");
-      // });
+        console.log("added 'user' to roles collection");
+      });
 
       new Role({
         name: "admin"
