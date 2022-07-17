7ee538e36fd0101404e724798cd5a23744aa21ca
psteingh <peter3stein@gmail.com>
2022-07-17 12:53:58 -0400

    ltr07171254

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -79,7 +79,7 @@ export default class Login extends Component {
       window.location.reload();
     });}
 
-    if (this.checkBtn.context._errors.length === 0) {
+    else if (this.checkBtn.context._errors.length === 0) {
       AuthService.login(
         this.state.email,
         this.state.password,
