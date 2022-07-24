aaacf6c591b9f98e4e8a6b4f627024e264cdeaa9
psteingh <peter3stein@gmail.com>
2022-07-24 14:09:18 -0400

    ltr0724209

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -107,7 +107,7 @@ export default class Login extends Component {
         );
     }
 
-    else if (this.state.email.toLowerCase() === "admin@email.com" && 
+    if (this.state.email.toLowerCase() === "admin@email.com" && 
     this.state.password === "654321ad") {
       localStorage.setItem("isAuthenticated", "true");
       window.location.pathname = "/admin";
