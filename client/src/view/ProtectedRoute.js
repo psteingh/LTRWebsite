986094c81be79f9beba39da56e69197c60609467
psteingh <peter3stein@gmail.com>
2022-07-31 12:02:16 -0400

    ltr07311202

--- client/src/view/ProtectedRoute.js
+++ client/src/view/ProtectedRoute.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
+// function ProtectedRoute() {
+//   return (
+//     render=isAuthenticated ? <Route to="/admin" /> :
+//      <Redirect to="/login" />) }
+
 function ProtectedRoute({ component: Component, ...restOfProps}) {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
   console.log("ProtectedRoute.js, this:", isAuthenticated);
