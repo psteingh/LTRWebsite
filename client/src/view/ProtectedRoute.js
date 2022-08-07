25e7910ea592cba709f377cee443a70dcfb5924d
psteingh <peter3stein@gmail.com>
2022-08-07 13:36:02 -0400

    ltr0807136

--- client/src/view/ProtectedRoute.js
+++ client/src/view/ProtectedRoute.js
@@ -2,22 +2,15 @@ import React, { Component } from "react";
 import { Redirect, Route } from "react-router-dom";
 
 export default class ProtectedRoute extends Component {
-
   render() {
-    const { component: Component, ...restOfProps } = this.props;
+    const { component: Component } = this.props;
     const isAuthenticated = localStorage.getItem("isAuthenticated");
     console.log("ProtectedRoute.js", isAuthenticated);
     
     return (
-      <div>
-      
-        <Route {...restOfProps}
-          render={(props) => ( isAuthenticated ?
-            <Component {...props} /> :
-            <Redirect to="/" />
+        <Route exact path="/admin"
+          render={() => (isAuthenticated ?
+            <Component /> :
+            <Redirect to="/login" />
           )} />
-
-      </div>
-    );
-  }
-}
+  )}}
