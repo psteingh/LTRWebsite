ec06dfbce3723fe714996b3b4176636f722e8da1
psteingh <peter3stein@gmail.com>
2022-08-07 10:13:34 -0400

    ltr08071013

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -69,16 +69,16 @@ export default class Login extends Component {
 
     if (this.state.email.toLowerCase() === "admin@email.com" && 
     this.state.password === "654321ad") {
-      AuthService.login(
-        this.state.email,
-        this.state.password,
-        this.state.currentUser,
-        ).then(
-        () => {
+      // AuthService.login(
+      //   this.state.email,
+      //   this.state.password,
+      //   this.state.currentUser,
+      //   ).then(
+      //   () => {
       localStorage.setItem("isAuthenticated", "true");
       this.props.history.push("/admin");
       window.location.reload();
-    } ) }
+    }
 
     else if (this.checkBtn.context._errors.length === 0) {
       AuthService.login(
