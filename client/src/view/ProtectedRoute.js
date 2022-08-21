b5f3ec28b5c5913e6fe6eb8cc7654df13662fc3d
psteingh <peter3stein@gmail.com>
2022-08-21 14:17:38 -0400

    ltr0821217

--- client/src/view/ProtectedRoute.js
+++ client/src/view/ProtectedRoute.js
@@ -3,26 +3,20 @@ import React, { Component } from "react";
 
 import AuthService from "../services/auth.service";
 
-import Admin from "../view/admin.component";
-// import Contact from "../home/contact";
+// import Admin from "../view/admin.component";
+import Contact from "../home/contact";
 
 export default class ProtectedRoute extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: AuthService.getCurrentUser()
-    };
-  }
-  
   render() {
   
   const Test = this.props.component;
   // console.log("ProtectedRoute.js Component:", Component);
 
-  const isAuthenticated = localStorage.getItem("isAuthenticated");
+  const isAuthenticated = AuthService.getCurrentUser;
+
+  // const isAuthenticated = localStorage.getItem("isAuthenticated");
   console.log("ProtectedRoute.js isAuthenticated:", isAuthenticated);
 
   return isAuthenticated ? ( <Test /> ) : (
-      <Admin /> );
+      <Contact /> );
 } }
