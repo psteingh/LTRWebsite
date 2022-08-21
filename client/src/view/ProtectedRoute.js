e38de9d67e40f4e41bd648f3b8234afc66f87c10
psteingh <peter3stein@gmail.com>
2022-08-21 13:20:44 -0400

    ltr0821120

--- client/src/view/ProtectedRoute.js
+++ client/src/view/ProtectedRoute.js
@@ -12,6 +12,6 @@ export default class ProtectedRoute extends Component {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
   console.log("ProtectedRoute.js isAuthenticated:", isAuthenticated);
 
-  return ( !isAuthenticated ? ( <Contact /> ) : (
+  return ( isAuthenticated ? ( <Contact /> ) : (
       <Admin /> ))
 } }
