03c91176cd686d4e79bb8b64e2a608781ad54691
psteingh <peter3stein@gmail.com>
2022-08-21 13:46:55 -0400

    ltr0821146

--- client/src/view/ProtectedRoute.js
+++ client/src/view/ProtectedRoute.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 // import { Redirect } from "react-router-dom";
 
+import AuthService from "../services/auth.service";
+
 import Admin from "../view/admin.component";
 // import Contact from "../home/contact";
 
 export default class ProtectedRoute extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: AuthService.getCurrentUser()
+    };
+  }
+  
   render() {
+  
   const Test = this.props.component;
   // console.log("ProtectedRoute.js Component:", Component);
 
