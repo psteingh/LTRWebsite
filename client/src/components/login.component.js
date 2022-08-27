f44299c738901b59fe75a0016efbc2cd4e9361a7
psteingh <peter3stein@gmail.com>
2022-08-27 11:44:45 -0400

    ltr08271144

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -90,6 +90,7 @@ export default class Login extends Component {
         ).then(
         () => {
           // localStorage.setItem("isAuthenticated", "false");
+          localStorage.clear();
           this.props.history.push("/ltrlies");
           window.location.reload();
         },
