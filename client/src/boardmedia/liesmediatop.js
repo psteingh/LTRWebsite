a966a15781cce7c134bb524bb2a3c648851affe6
psteingh <peter3stein@gmail.com>
2022-09-04 12:42:58 -0400

    ltr09041243

--- client/src/boardmedia/liesmediatop.js
+++ client/src/boardmedia/liesmediatop.js
@@ -2,43 +2,16 @@ import React from "react";
 
 function LiesMediaTop(){
   return(
-  <div>
   <div className="boardtop-container">
-    <div className="lies-title">
+    <div className="board-title">
     Lies in the Media
     </div>
-    <div className="lies-mid">
+    <div className="board-mid">
     The umbrella term for Media includes: Books, Games, Movies, Music, TV Shows
     </div>
-    <div className="lies-bottom">
+    <div className="board-bottom">
     Lies that you see in the media
     </div>
-  </div>
-
-  <div className="board-group-item">
-    <div className="lies-title">
-    Movie
-    </div>
-    <div className="lies-mid">
-    Easy A
-    </div>
-    <div className="lies-bottom">
-    "Acclerated velocity of terminological inexactitude"
-    </div>
-  </div>
-
-  <div className="board-group-item">
-    <div className="lies-title">
-    Movie
-    </div>
-    <div className="lies-mid">
-    Jupiter Ascending
-    </div>
-    <div className="lies-bottom">
-    “Lies are a necessity, they are the source of meaning”
-    </div>
-  </div>
-
   </div>
   
   );
