2060e742c938acbd93b1e16b855446adc2ad8ea1
psteingh <peter3stein@gmail.com>
2022-09-10 13:55:33 -0400

    ltr0910155

--- client/src/boardaboutlies/aboutliestop.js
+++ client/src/boardaboutlies/aboutliestop.js
@@ -2,32 +2,16 @@ import React from "react";
 
 function AboutLiesTop(){
   return(
-  <div>
-
   <div className="boardtop-container">
-    <div className="lies-title">
+    <div className="board-title">
     Purpose of Lies
     </div>
-    <div className="lies-mid">
+    <div className="board-mid">
     There are many conditions which determine the purpose of lying
     </div>
-    <div className="lies-bottom">
+    <div className="board-bottom">
     Our society has fully incorporated lying so why not keep track of them for yourself
     </div>
-  </div>
-
-  <div className="board-group-item">
-    <div className="lies-title">
-    Question of Lying
-    </div>
-    <div className="lies-mid">
-    Is omission of fact a lie?
-    </div>
-    <div className="lies-bottom">
-    Depending on the situation, leaving out details does not have to be a lie
-    </div>
-  </div>
-
   </div>
 
   );
