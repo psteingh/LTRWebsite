e223176dd815e1d5a6f3173961eff0e0bc0598e6
psteingh <peter3stein@gmail.com>
2022-09-24 14:19:54 -0400

    ltr0924220

--- controllers/lierBoard.controller.js
+++ controllers/lierBoard.controller.js
@@ -3,16 +3,23 @@ const LtrLie = db.ltrlies;
 
 // Retrieve and Sort all published LtrLies
 exports.findAllPublished = (req, res) => {
-  // const arr = [ 'name', 'subject', 'stuff'];
+  // const arr = ['name'];
+  // const arr = ['name', 'id', 'stuff'];
+  // const arr = [];
+  // const arr = []
+  // arr({name: 1})
+  // arr({stuff: 2});
+  // arr.sort((a, b) => a.name - b.name);
+  // arr.sort((a, b) => a.name.localeCompare(b.name, 'en', {
+  // sensitivty: 'base' }));
 
-  const arr = ['stuff'];
-  const sorted = arr.sort((a, b) => 
-    a.localeCompare(b));
+  const arr = { name: 1 };
+  console.log("lierboard.controller.jsA:");
   
-  LtrLie.find({published: true}, sorted)
-    
+  LtrLie.find({published: true})
+    .sort(arr)
     .then(data => {
-    console.log("lierboard.controller.jsA:");
+    console.log("lierboard.controller.jsB:");
     res.send(data);
     })
 
