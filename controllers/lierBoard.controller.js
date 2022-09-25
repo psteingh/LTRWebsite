afef28148f9b4aa117b6fa0953358249688d9e15
psteingh <peter3stein@gmail.com>
2022-09-25 08:48:36 -0400

    ltr0925848

--- controllers/lierBoard.controller.js
+++ controllers/lierBoard.controller.js
@@ -3,23 +3,12 @@ const LtrLie = db.ltrlies;
 
 // Retrieve and Sort all published LtrLies
 exports.findAllPublished = (req, res) => {
-  // const arr = ['name'];
-  // const arr = ['name', 'id', 'stuff'];
-  // const arr = [];
-  // const arr = []
-  // arr({name: 1})
-  // arr({stuff: 2});
-  // arr.sort((a, b) => a.name - b.name);
-  // arr.sort((a, b) => a.name.localeCompare(b.name, 'en', {
-  // sensitivty: 'base' }));
-
   const arr = { name: 1 };
-  console.log("lierboard.controller.jsA:");
   
   LtrLie.find({published: true})
+    .collation({locale: "en"})
     .sort(arr)
     .then(data => {
-    console.log("lierboard.controller.jsB:");
     res.send(data);
     })
 
--- models/ltrlie.model.js
+++ models/ltrlie.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const LtrLieSchema = new mongoose.Schema({
   name: String,
+  collation: String,
   subject: String,
   stuff: String,
   currentUser: String,
