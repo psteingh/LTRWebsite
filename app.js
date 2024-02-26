3081d8e8f9264e88de4902e27654d7e124b58173
psteingh <peter3stein@gmail.com>
2024-02-26 10:56:28 -0500

    ltrws02261056

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const db = require("./models");
 
 db.mongoose
   .connect(
-    process.env.MONGODB_URI, { useNewUrlParser: true })
+    process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("app.js.db.mongoose");
   })
